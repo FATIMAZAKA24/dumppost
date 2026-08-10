cf9a313cbb3633f59e797224104ecef7407c3c2e
Fatima Zaka <fatimazaka427@gmail.com>
2026-08-10 16:25:31 +0500

    cmodel updated

--- src/app/api/generate/route.ts
+++ src/app/api/generate/route.ts
@@ -18,7 +18,8 @@ async function groqCall(
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      model: 'llama-3.3-70b-versatile',
+      //model: 'llama-3.3-70b-versatile',
+      model: 'openai/gpt-oss-120b',
       messages,
       temperature,
       max_tokens,
